61ee4b97a362b05b48c2fd3acac6f77fe99212ad
caijianhaoa <1784581164@qq.com>
2022-07-13 16:03:04 +0800

    first commit

---
+++ demo/which-react.js
@@ -0,0 +1,19 @@
+// import React, {
+//   useReducer,
+//   useState,
+//   Component,
+//   useEffect,
+//   useLayoutEffect,
+// } from "react";
+// import ReactDOM from "react-dom";
+
+import {
+  Component,
+  useReducer,
+  useState,
+  useEffect,
+  useLayoutEffect,
+} from '../src/react'
+import ReactDOM from '../src/react-dom'
+
+export { ReactDOM, Component, useReducer, useState, useEffect, useLayoutEffect }
